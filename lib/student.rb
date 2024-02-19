bd31b92a13e94f66418ac6ae225aff210f28908f
Ashley <ashuhleyt@gmail.com>
2024-02-18 19:53:20 -0800

    for funsies

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
 class Student
+  attr_reader :name, 
+              :age, 
+              :scores
 
+  def initialize(data)
+    @name = data[:name]
+    @age = data[:age]
+    @scores = []
+  end
+
+  def log_score(number)
+    @scores << number
+  end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,10 +2,21 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  it "exists" do
-    morgan = Student.new({name: "Morgan", age: 21})
-    expect(morgan).to be_instance_of(Student)
+  before :each do 
+    @morgan = Student.new({name: "Morgan", age: 21})
   end
 
-  
+  it "exists with attributes" do
+    expect(@morgan).to be_instance_of(Student)
+    expect(@morgan.name).to eq("Morgan")
+    expect(@morgan.age).to eq(21)
+  end
+
+  it 'has no scores as an empty array' do 
+    expect(@morgan.scores).to eq([])
+
+    @morgan.log_score(89)
+    @morgan.log_score(78)
+    expect(@morgan.scores).to eq([89, 78])
+  end
 end
